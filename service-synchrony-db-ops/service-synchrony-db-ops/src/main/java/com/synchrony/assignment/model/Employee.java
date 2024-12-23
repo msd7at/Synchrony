d9ff2bcba53a49f068a673ca90922895a70576e2
Anurag Tiwari <anurag.7140@gmail.com>
2024-12-23 10:12:23 +0530

    Added : Employee class

---
+++ service-synchrony-db-ops/service-synchrony-db-ops/src/main/java/com/synchrony/assignment/model/Employee.java
@@ -0,0 +1,49 @@
+package com.synchrony.assignment.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String position;
+    private double salary;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
